badbc3cb8f0a26d122aa33ca3f3fea0d480d5bb2
juarez-christian <christian.majs@gmail.com>
2018-10-10 15:20:12 -0500

    W2 assignment 1

---
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -0,0 +1,23 @@
+/*
+ * The main() class file for the cityOfAaron project
+ * CIT-260
+ * Fall 2018
+ *  Team members: Lo Ching, Kachia Vang, Christian Juarez
+ */
+
+package cityofaaron;
+
+/**
+ *
+ * @author _Chris_
+ */
+public class CityOfAaron {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
