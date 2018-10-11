9941bb6ace5ac98e4c55b44f5e39b906efbb05bc
juarez-christian <christian.majs@gmail.com>
2018-10-10 19:08:28 -0500

    W4 assignment 1

---
+++ cityOfAaron/src/model/TeamMember.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author _Chris_
+ */
+public enum TeamMember 
+{
+    // the members of the enumeration
+    
+    
+    // data members
+    private String name;
+    private String title;
+       
+    // The getName() method
+    // Purpose: returns the value stored in name
+    // Parameters: none
+    // Returns: the value stored in name
+    public String getName()
+    {
+        return name;
+    }
+
+    // The getTitle() method
+    // Purpose: returns the value stored in title
+    // Parameters: none
+    // Returns: the value stored in title
+    public String getTitle()
+    {
+        return title;
+    
+    
+    // public constructor 
+    // purpose: initializes de values of name and title
+    // parameters: the name and title of the team member
+    // returns: none
+    
+    TeamMember(String _name, String _title)
+    {
+        name = _name;
+        title = _title;
+    }
+
+}
+    
