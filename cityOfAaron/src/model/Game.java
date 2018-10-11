636233fe187ffd2a5fd719b083f0ab24175326ab
juarez-christian <christian.majs@gmail.com>
2018-10-10 19:08:44 -0500

    W4 assignment 1

--- cityOfAaron/src/model/Game.java
+++ cityOfAaron/src/model/Game.java
@@ -9,6 +9,22 @@ package model;
  *
  * @author _Chris_
  */
-public class Game {
+import java.io.Serializable;
+
+
+public class Game implements Serializable
+{
+    private Player thePlayer;
     
+    public Game() {};
+    
+    public Player getthePlayer()
+    {
+        return this.thePlayer;
+    }
+            
+    public void setthePlayer(Player _thePlayer)
+    {
+        thePlayer = _thePlayer;
+    }        
 }
