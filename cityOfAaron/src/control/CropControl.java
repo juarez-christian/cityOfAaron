4f3f72c7be9d554ca5f03a85a112006d8c0d8d49
Ching Lo <>
2018-10-26 23:19:55 -0500

    10/26/18 added the code and tests for feedPeople

--- cityOfAaron/src/control/CropControl.java
+++ cityOfAaron/src/control/CropControl.java
@@ -17,84 +17,44 @@ import model.CropData;
 
 public class CropControl {
  
- 
-    
- /* Generate a random number for the price of an acre of land. 
+/* Generate a random number for the price of an acre of land. 
     Make this value between 17 and 27 bushels per acre.*/ 
- /*   
-    // constants
-     private static final int LAND_BASE = 17;
-     private static final int LAND_RANGE = 10;
+/*   
+// constants
+   private static final int LAND_BASE = 17;
+   private static final int LAND_RANGE = 10;
         
-     // random number generator
-     private Random random = new Random();
+// random number generator
+   private Random random = new Random();
      
-     
-     // calcLandCost() method
-    // Purpose: Calculate a random land cost between 17 and 26 bushels/acre
-    // Parameters: none
-    // Returns: the land cost
-   public static int calcLandCost()
+// calcLandCost() method
+// Purpose: Calculate a random land cost between 17 and 26 bushels/acre
+// Parameters: none
+// Returns: the land cost
+    
+    public static int calcLandCost()
     {
         int landPrice = random.nextInt(LAND_RANGE) + LAND_BASE;  
         return landPrice;            
     }
   
  */
-    
-    
-
-// The buyLand method
-// Purpose: To buy land
-// Parameters: the price of the land, the number of acres to buy,
-//     	and a reference to a CropData object
-// Returns: the total acres owned after the purchase
-// Pre-conditions: acres to buy must be positive
-//     	and <= the number of wheat owned
-    
-    // method signature
-    public static int buyLand(int landPrice, int acresToBuy, CropData cropData)
-    {
-    // if acresToBuy < 0, return -1
-        if (acresToBuy < 0)
-        return -1;
-    // if acresToBuy > wheatInStore,  return -1
-    int owned = cropData.getWheatInStore();
-        if ((acresToBuy * landPrice) > cropData.getWheatInStore())
-        return -1;
-    // acresOwned = acresOwned + acresToBuy
-        owned += acresToBuy;
-        cropData.setAcresOwned(owned);
-    // wheatInStore = wheatInStore - (acresToBuy x landPrice)
-    int stored = cropData.getWheatInStore(); 
-        stored -= (acresToBuy * landPrice);    
-        cropData.setWheatInStore(stored);
-    // return acresOwned
-        return owned;
-    }
-
-  
-
-
-
-
-
 
 // sellLand method
-    // Purpose: Sell land  - subtracts from the acres owned
-    // Parameters: the price of land, the number of acres to sell, and
-    //     a reference to a CropData object 
-    // Pre-conditions: acresToSell >= 0 and acresToSell <= acresOwned
-    // Returns: the number of acres owned after the sale
+// Purpose: Sell land  - subtracts from the acres owned
+// Parameters: the price of land, the number of acres to sell, and
+//     a reference to a CropData object 
+// Pre-conditions: acresToSell >= 0 and acresToSell <= acresOwned
+// Returns: the number of acres owned after the sale
     
     // method signature
     public static int sellLand(int landPrice, int acresToSell, CropData cropData)
     {
-          //if acresToSell < 0, return -1
+        //if acresToSell < 0, return -1
           if(acresToSell < 0)
               return -1;
         
-         //if acresToSell > acresOwned, return -1
+        //if acresToSell > acresOwned, return -1
         int owned = cropData.getAcresOwned();
         if(acresToSell > owned)
              return -1;
@@ -110,12 +70,39 @@ public class CropControl {
         
         //return acresOwned
         return owned;
-    }
-
-
-
+    }    
     
-    // The setOffering method
+// The buyLand method
+// Purpose: To buy land
+// Parameters: the price of the land, the number of acres to buy,
+//     	and a reference to a CropData object
+// Returns: the total acres owned after the purchase
+// Pre-conditions: acres to buy must be positive
+//     	and <= the number of wheat owned
+    
+    // method signature
+    public static int buyLand(int landPrice, int acresToBuy, CropData cropData)
+    {
+        // if acresToBuy < 0, return -1
+        if (acresToBuy < 0)
+        return -1;
+        // if acresToBuy > wheatInStore,  return -1
+        int owned = cropData.getWheatInStore();
+        if ((acresToBuy * landPrice) > cropData.getWheatInStore())
+        return -1;
+        // acresOwned = acresOwned + acresToBuy
+        owned += acresToBuy;
+        cropData.setAcresOwned(owned);
+        // wheatInStore = wheatInStore - (acresToBuy x landPrice)
+        int stored = cropData.getWheatInStore(); 
+        stored -= (acresToBuy * landPrice);    
+        cropData.setWheatInStore(stored);
+        // return acresOwned
+        return owned;
+    }
+    
+// author Christian Juarez
+// the setOffering method 
 // Purpose: To allocate the offering
 // Parameters: a reference to a payOffering object
 //     	and a reference to a CropData object
@@ -128,17 +115,50 @@ public class CropControl {
     // method signature
     public static int setOffering(int payOffering, CropData cropData)
     {
-    // if payoffering < 0, return -1
+        // if payoffering < 0, return -1
         if (payOffering < 0)
         return -1;
-    // if payoffering > 100 return -1
-    if (payOffering > 100)
+        // if payoffering > 100 return -1
+        if (payOffering > 100)
         return -1;
-    // return the percentage of offering
-    return payOffering;
+        // return the percentage of offering
+        return payOffering;
     }
     
+// author Ching Lo
+// the feedPeople method 
+// Purpose: To set aside bushels of wheat to feed people
+// @param the number of bushels of wheat to set aside
+// @param a reference to a CropData object
+// @return the number of wheat for people/food 
+// @return the number of wheat in store
+// Pre-conditions: wheat to set aside for food must be positive
+//    and <= the number of wheat in store
 
+    // method signature
+    public static int feedPeople(int wheatForFood, CropData cropData)
+    { 
+        //if wheatForFood < 0, return -1
+        if(wheatForFood < 0)
+            return -1;
+        // if wheatForFood > wheatInStore,  return -1
+        int wheatInStore = cropData.getWheatInStore();
+        if(wheatForFood > wheatInStore)
+            return -1;
+        
+        // wheatInStore = wheatInStore - wheatForPeople
+        // updates wheatInStore variable
+        wheatInStore -= wheatForFood;
+        cropData.setWheatInStore(wheatInStore);
+        
+        // wheat for people same as wheat for food
+        // wheatForFood = newly set aside wheat for people + wheat already stored for people
+        // update cropData.wheatForFood variable
+        wheatForFood += cropData.getWheatForFood();
+        cropData.setWheatForFood(wheatForFood);
+        
+        return wheatForFood;
+    }
     
 }
 
