c2f0643549eec0dc87008288be9059ef1317b687
Ching Lo <>
2018-11-07 20:08:14 -0600

    11/7/18 CL changed howManyPeopleStarved = cropData.getNumStarved();

--- cityOfAaron/src/view/CropView.java
+++ cityOfAaron/src/view/CropView.java
@@ -21,25 +21,23 @@ public class CropView {
     static private CropData cropData = game.getcropData();
 
     /**
-     * The runCropView method() 
-     * Purpose: runs the methods to manage the crops
-     * Game Parameters: none 
-     * Returns: none
+     * The runCropView method() Purpose: runs the methods to manage the crops
+     * Game Parameters: none Returns: none
      * =================================================================
      */
     public static void runCropView() {
         // call the buyLandView( ) method
         buyLandView();
-        
+
         // call the sellLandView( ) method
         sellLandView();
 
         // call the feedPeopleView( ) method
         feedPeopleView();
-       
+
         // call the feedPeopleView( ) method
         plantCropsView();
-        
+
         // call the feedPeopleView( ) method
         displayCropsReportView();
     }
@@ -90,42 +88,43 @@ public class CropView {
         // output how much land we now own
         System.out.format("You now own %d acres of land. ", cropData.getAcresOwned());
     }
-    
+
     // The feedPeopleView method
     // Purpose: interface with the user input for feeding the people
     // Parameters: none 
     // Returns: none
     // Author: Ching Lo
-    public static void feedPeopleView(){
+    public static void feedPeopleView() {
         // Prompt the user to enter the number of bushels to feed people
         System.out.print("How many bushels of grain do you want to give to the people? ");
-        
+
         // Get the user’s input and save it.
         int wheatForFood;
-        wheatForFood = keyboard.nextInt ();
+        wheatForFood = keyboard.nextInt();
 
         // Call the feedPeople( ) method in the control layer 
         CropControl.feedPeople(wheatForFood, cropData);
 
         // output how much bushels of wheat we have left
         System.out.format("You now have %d bushels of wheat. ", cropData.getWheatInStore());
-        
+
     }
-    
- /**
-     * plantCropsView method
-     * Purpose: Display acres owned and wheat in store and then prompt user for amount
-     * they wish to use to plant crops
-     */
+
+    // plantCropsView method
+    // Purpose: Display acres owned and wheat in store 
+    //   and then prompt user for amount they wish to use to plant crops
+    // Parameters: none
+    // Returns: none
+    // Author: Kachia Vang
     public static void plantCropsView() {
         //display current land, wheat, and population
         System.out.format("\n\nHow much land needed for crops?\nAcres of land owned: %d\n", cropData.getAcresOwned());
         System.out.format("Bushels of Wheat in storehouse: %d\n", cropData.getWheatInStore());
         System.out.format("Current city population: %d\n", cropData.getPopulation());
         //display conditions needed to care for crops and prompt user for input
-        System.out.println("\n1 bushel of wheat can plant 2 acres, " +
-                           "while 1 person can take care of 10 acres planted.\n" +
-                           "How many acres of land do you wish to plant? ");
+        System.out.println("\n1 bushel of wheat can plant 2 acres, "
+                + "while 1 person can take care of 10 acres planted.\n"
+                + "How many acres of land do you wish to plant? ");
         //get input and save
         int toPlant;
         toPlant = keyboard.nextInt();
@@ -135,35 +134,31 @@ public class CropView {
         System.out.format("\nYou have planted %d acres of land for next year's harvest.", cropData.getAcresPlanned());
         System.out.format("\nCurrent wheat in storehoues: %d\n", cropData.getWheatInStore());
     }
-    
-    
+
     // The displayCropsReportView method
     // Purpose: interface with the user input for display a report of the crops
     // Parameters: none 
     // Returns: none
     // Author: Christian Juarez
-    
     public static void displayCropsReportView() {
-        
-int theYearNumber = cropData.getYear();
-int howManyPeopleStarved = cropData.getHarvest();
-int howManyPeopleComeToTheCity = cropData.getNewPeople();
-int theCurretntPopulation = cropData.getPopulation();
-int theNumberOfAcresOfLandOwned = cropData.getAcresOwned();
-int theNumberOfBushelsPerAcreHarvested = cropData.getWheatForFood();
-int theTotalNumberOfBushelsOfWheatHarvested = cropData.getAcresPlanned();
-int theNumberOfBushelsOfWheatInStore = cropData.getWheatInStore();
-    
 
-System.out.format("Year:" + theYearNumber + " %n");
-System.out.format("People starved: " + howManyPeopleStarved + " %n");
-System.out.format("New People: " + howManyPeopleComeToTheCity + " %n");
-System.out.format("Population: " + theCurretntPopulation + " %n");
-System.out.format("Land owned by the city: " + theNumberOfAcresOfLandOwned + " %n");
-System.out.format("Bushels per acre harvested: " + theNumberOfBushelsPerAcreHarvested + " %n");
-System.out.format("Bushels of wheat harvested: " + theTotalNumberOfBushelsOfWheatHarvested + " %n");
-System.out.format("Bushels of wheat in store: " + theNumberOfBushelsOfWheatInStore + " %n");
-         }
-    
-    
+        int theYearNumber = cropData.getYear();
+        int howManyPeopleStarved = cropData.getNumStarved();
+        int howManyPeopleComeToTheCity = cropData.getNewPeople();
+        int theCurrentPopulation = cropData.getPopulation();
+        int theNumberOfAcresOfLandOwned = cropData.getAcresOwned();
+        int theNumberOfBushelsPerAcreHarvested = cropData.getHarvest();
+        int theTotalNumberOfBushelsOfWheatHarvested = cropData.getAcresPlanned();
+        int theNumberOfBushelsOfWheatInStore = cropData.getWheatInStore();
+
+        System.out.format("Year:" + theYearNumber + " %n");
+        System.out.format("People starved: " + howManyPeopleStarved + " %n");
+        System.out.format("New People: " + howManyPeopleComeToTheCity + " %n");
+        System.out.format("Population: " + theCurrentPopulation + " %n");
+        System.out.format("Land owned by the city: " + theNumberOfAcresOfLandOwned + " %n");
+        System.out.format("Bushels per acre harvested: " + theNumberOfBushelsPerAcreHarvested + " %n");
+        System.out.format("Bushels of wheat harvested: " + theTotalNumberOfBushelsOfWheatHarvested + " %n");
+        System.out.format("Bushels of wheat in store: " + theNumberOfBushelsOfWheatInStore + " %n");
+    }
+
 }
