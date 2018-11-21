1a7859dfcc7f7ec5214437ae07caccdc3a908dad
Ching Lo <>
2018-11-21 01:34:56 -0600

    W10 added createMap and location objects

--- cityOfAaron/src/model/Game.java
+++ cityOfAaron/src/model/Game.java
@@ -10,12 +10,24 @@ package model;
  * @author _Chris_
  */
 import java.io.Serializable;
+import java.util.ArrayList;
 
 
 public class Game implements Serializable
 {
     private Player thePlayer;
     private CropData cropData;
+    private Map theMap;
+    
+    /* 
+    * Nov 21,2018 Added variables for Animals List, Tools List and Provisions List
+    */
+    //variable for keeping a reference to the animals ListItem object
+    private ArrayList<ListItem> animals = null;
+    //variable for keeping a reference to the animals ListItem object
+    private ArrayList<ListItem> tools = null;
+    //variable for keeping a reference to the animals ListItem object
+    private ArrayList<ListItem> provisions = null;
     
     public Game() {};
     
@@ -29,7 +41,7 @@ public class Game implements Serializable
         thePlayer = _thePlayer;
     }        
 
-   public CropData getcropData()
+    public CropData getcropData()
     {
         return this.cropData;
     }
@@ -38,5 +50,45 @@ public class Game implements Serializable
     {
         cropData = _cropData;
     }       
-       
+
+    public Map gettheMap()
+    {
+        return this.theMap;
+    }
+
+    public void settheMap(Map _theMap) 
+    {  
+        theMap = _theMap;
+    }
+
+    public ArrayList<ListItem> getAnimals()
+    {
+        return animals;
+    }
+           
+    public void setAnimals(ArrayList<ListItem> animals) 
+    {
+        this.animals = animals;
+    }
+    
+    public ArrayList<ListItem> getTools()
+    {
+        return tools;
+    }
+           
+    public void setTools(ArrayList<ListItem> tools) 
+    {
+        this.tools = tools;
+    }
+    
+    public ArrayList<ListItem> getProvisions()
+    {
+        return provisions;
+    }
+           
+    public void setProvisions(ArrayList<ListItem> provisions) 
+    {
+        this.provisions = provisions;
+    }
+    
 }
